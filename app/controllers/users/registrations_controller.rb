8c75122709b96a11f9e449d9a01852c9cdfe07a5
Rick Mason <rickymm3@gmail.com>
2014-09-05 21:17:19 -0400

    fixed user login problem

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 module Users
-
   class RegistrationsController < Devise::RegistrationsController
+    include CreateUserHelper
     # before_filter :configure_permitted_parameters, if: :devise_controller?
     def create
       super
@@ -8,32 +8,6 @@ module Users
         create_new_user(@user)
       end
 
-    end
-
-    private
-
-    def create_new_user(user)
-      add_user_structures(user)
-      add_user_stats(user)
-    end
-
-    def add_user_structures(user)
-      # checkin = Checkin.new user: @user, location: @location, source_id: source_id
-      structures = Structure.all
-      structures.each do |s|
-        us = UserStructure.new user: user, structure: s, num: 1
-        us.save
-      end
-    end
-
-    def add_user_stats(user)
-      user.gold = 300
-      user.mana = 300
-      user.orb_count = 3
-      user.land = 5
-      user.touch(:collect_gold)
-      user.touch(:collect_mana)
-      user.save
     end
 
     # def configure_permitted_parameters
--- app/helpers/create_user_helper.rb
+++ app/helpers/create_user_helper.rb
@@ -1,4 +1,29 @@
 module CreateUserHelper
 
+  def create_new_user(user)
+    add_user_structures(user)
+    add_user_stats(user)
+  end
 
+  def add_user_structures(user)
+    # checkin = Checkin.new user: @user, location: @location, source_id: source_id
+    structures = Structure.all
+    structures.each do |s|
+      us = UserStructure.new user: user, structure: s, num: 1
+      us.save
+    end
+  end
+
+  def add_user_stats(user)
+    user.gold = 300
+    user.mana = 300
+    user.orb_count = 3
+    user.land = 5
+    user.explore_end = Time.now-1.minute
+    user.touch(:last_collect_gold_at)
+    user.touch(:last_collect_mana_at)
+    user.gold_collect_again_at = Time.now+15.minutes
+    user.mana_collect_again_at = Time.now+15.minutes
+    user.save
+  end
 end
